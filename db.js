5e12ccf8b8946a383d039ab900ca617bb924b5ae
MohammadWajhiUddin <49415375+MohammadWajhiUddin@users.noreply.github.com>
2024-06-08 00:50:47 +0500

    Update db.js

--- db.js
+++ db.js
@@ -2,14 +2,14 @@ const {MongoClient }  = require('mongodb')
 
 let dbConnection
 
-const uri = "mongodb+srv://heartdiseasepredictor:4HIcrbdpZrtZCen3@heartdiseasepredictor.pyz2hy7.mongodb.net/?retryWrites=true&w=majority&appName=heartdiseasePredictor"
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true ,    serverSelectionTimeoutMS: 60000});
 
 module.exports ={
     connectToDb: (cb) => {
 
       
-        client.connect('mongodb+srv://heartdiseasepredictor:4HIcrbdpZrtZCen3@heartdiseasepredictor.pyz2hy7.mongodb.net/?retryWrites=true&w=majority&appName=heartdiseasePredictor')
+        MongoClient.connect('mongodb+srv://heartdiseasepredictor:4HIcrbdpZrtZCen3@heartdiseasepredictor.pyz2hy7.mongodb.net/?retryWrites=true&w=majority&appName=heartdiseasePredictor',
+            { useNewUrlParser: true,}
+        )
             .then((client)=>{
                     dbConnection  = client.db('heartcaredb');
                     return cb()
